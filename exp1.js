853ddebde3961680eabc3731d0f45c6d6949226d
Naman Gupta <naman6023@gmail.com>
2026-03-16 14:54:51 +0530

    11/07/2026

---
+++ exp1.js
@@ -0,0 +1,38 @@
+function calculateResult(){
+    const n = document.getElementById("subjects").value ; 
+    let total = 0 ; 
+    for(let i = 0 ; i < n ; i++){
+        let x = parseFloat(prompt("Enter the Subject no." + (i+1))) ;
+        total += x ; 
+
+    }
+    let avg = total / n ; 
+    let grade ; 
+    if(avg > 90){
+        grade = "A+" ;
+    }else{
+        if(avg > 80){
+            grade = "B+" ;
+        }else{
+            if(avg > 70){
+                grade = "C+" ;
+            }
+            else{
+                if(avg >60){
+                    grade = "D+ " ;
+                }
+                else{
+                    grade = "Khtm" ;
+                }
+            }
+        }
+    }
+    let status ; 
+    if(avg > 60){
+        status = "PASS" ;
+    }else{
+        status = "FAIL" ;
+    }
+    let result = document.getElementById("result").innerHTML = "Total Marks : " + total +"<br/>" + "average : " + avg.toFixed(2) + "<br/>" + "grade : " + grade + "<br/>" + "Status : " + status + "<br/>";
+
+}
---
+++ exp2.js
@@ -0,0 +1,25 @@
+let Employee = [] ;
+function addEmployee(){
+    let name = document.getElementById("name").value; 
+    let empID = document.getEmployeeById("ID").value ;
+    let salary = document.parseFloat(getEmployeeById("ID").value) ;
+    let dept = document.getEmployeeById("dept").value ;
+
+}
+
+let employee = {
+    name : name,
+    id: empID , 
+    salary : salary ,
+    department : dept ,
+} ;
+Employee.push(employee)
+alert("Added successfully") ;
+getElementById("name").value = "" ;
+getElementById("ID").value = "" ;
+getEmployeeById("salary").value = "" ;
+getEmployeeById("dept").value = "" ;
+
+function filterSalary(){
+    let filtered = employee
+}
